6b33bfb9e2aa5cf1e490563aa21e2eb0c39982dd
Antonin ROSA-MARTIN <antonin.rosa-martin@geode.science>
2021-12-23 11:58:31 +0100

    Typo in the dependencies

--- src/types/Classes/GraphFormatConverter.ts
+++ src/types/Classes/GraphFormatConverter.ts
@@ -348,7 +348,7 @@ export class GraphFormatConverter {
         const builder = new XMLBuilder(GraphFormatConverter.options);
 
         // Return the XML built and "fix" the <?xml ... /></?xml> to <?xml ... ?> and replace the backspace chars
-        return builder.build(root).replace(/><\/\?xml>/, "?>").replace("\b", "");
+        return builder.build(root).replace(/><\/\?xml>/, "?>").replace(new RegExp("\b", ""), "");
     }
 
     /**
